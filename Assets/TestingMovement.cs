847fb4fbab53d966f8ff1089d563ab2b28dcfba5
Peyton Hopkins <peytonlukehopkins@outlook.com>
2023-11-09 10:58:05 -0600

    Added basic movement mechanics and multiplayer functionality.

---
+++ Assets/TestingMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float horzWalk;
+    private float vertWalk;
+
+    private Vector2 walkDir;
+
+    public float speed = 5f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Screen.SetResolution(500, 500, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horzWalk = Input.GetAxisRaw("Horizontal");
+        vertWalk = Input.GetAxisRaw("Vertical");
+
+        walkDir = new Vector2(horzWalk, vertWalk).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = (walkDir * speed);
+    }
+}
